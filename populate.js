375e09df0a92584ba01b9880b178ead98f8e2a4b
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-04 20:49:20 -0700

    creating functions to generate csv files

--- populate.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const generateData = require('./populateData.js').generateData;
-
-generateData(100, fs.createWriteStream('generatedData3.json'), 'utf8', () => {
-  console.log('done');
-});
