7a5171d74cf330eed0a769894235fac0454da9d4
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-12 16:14:28 -0700

    ec2

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,4 @@
 // require('newrelic');
 const app = require('./index.js');
 
-app.listen(8080, () => { console.log('Apateez app listening on port 8080!'); });
+app.listen(80, () => { console.log('Apateez app listening on port 80!'); });
