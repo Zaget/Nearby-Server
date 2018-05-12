dacc2d5d95c66a3a4dec93945d97e9b706838ab6
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-12 16:32:14 -0700

    ec2

--- server/src/index.js
+++ server/src/index.js
@@ -31,11 +31,11 @@ app.get('/restaurants/:id', (req, res) => {
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" http://13.57.205.164:3004/restaurants/styles.css">
       </head>
       <body>
         <div id="nearby-app">${markup}</div>
-        <script src="bundle.js"></script>
+        <script src="http://13.57.205.164:3004/restaurants/bundle.js"></script>
       </body>
     </html>
   `);
