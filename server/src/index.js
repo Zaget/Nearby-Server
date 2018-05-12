9189a799e9b72aa469437a9a27874a79a2798ab8
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-12 16:23:23 -0700

    ec2

--- server/src/index.js
+++ server/src/index.js
@@ -7,43 +7,38 @@ const App = require('../../client/dist/bundle.js').default;
 
 const app = express();
 const React = require('react');
+const markup = renderToString(React.createElement(App));
 
-app.get('/', (req, res) => {
-  res.send('i hope this works')
-})
+app.use(bodyParser.json());
 
-// const markup = renderToString(React.createElement(App));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-// app.use(bodyParser.json());
+app.use('/restaurants', express.static(path.join(__dirname, '/../../client/dist')));
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+app.get('/api/restaurants/:id/nearby', query);
 
-// app.use('/restaurants', express.static(path.join(__dirname, '/../../client/dist')));
-
-// app.get('/api/restaurants/:id/nearby', query);
-
-// app.get('/restaurants/:id', (req, res) => {
-//   res.send(`
-//     <!DOCTYPE html>
-//     <html>
-//       <head>
-//         <title>Apateez Recommendations</title>
-//         <link rel="icon" href="https://s3-us-west-1.amazonaws.com/apateezassets/apateez-logo-small-red.jpeg" type="image/x-icon">
-//         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400" rel="stylesheet">
-//         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-//         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-//         <link rel="stylesheet" href="http://13.57.205.164:3004/restaurants/styles.css">
-//       </head>
-//       <body>
-//         <div id="nearby-app">${markup}</div>
-//         <script src="http://13.57.205.164:3004/restaurants/bundle.js"></script>
-//       </body>
-//     </html>
-//   `);
-// });
+app.get('/restaurants/:id', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Apateez Recommendations</title>
+        <link rel="icon" href="https://s3-us-west-1.amazonaws.com/apateezassets/apateez-logo-small-red.jpeg" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+        <link rel="stylesheet" href="http://13.57.205.164:3004/restaurants/styles.css">
+      </head>
+      <body>
+        <div id="nearby-app">${markup}</div>
+        <script src="http://13.57.205.164:3004/restaurants/bundle.js"></script>
+      </body>
+    </html>
+  `);
+});
 
 module.exports = app;
