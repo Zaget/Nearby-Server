07675d4fd7980c8f8173c277ebce7ac3997fd666
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-12 16:47:47 -0700

    ec2

--- server/src/psqlQuery.js
+++ server/src/psqlQuery.js
@@ -4,6 +4,7 @@ const client = new Pool({ database: 'zaget', host: 'zaget.cvjywnma6qrl.us-west-1
 
 const psqlQuery = (req, res) => {
   const placeId = parseInt(req.params.id, 10);
+  console.log(placeId);
   client.query(`select * from businesses where place_id = ${placeId}`, (err, data) => {
     console.log([data.rows[0]]);
   // client.query(`select * from nearby inner join businesses on ${placeId} = businesses.place_id or nearby.nearby1 = businesses.place_id or nearby.nearby2 = businesses.place_id or nearby.nearby3 = businesses.place_id or nearby.nearby4 = businesses.place_id or nearby.nearby5 = businesses.place_id or nearby.nearby6 = businesses.place_id where nearby.place_id = ${placeId}`, (err, data) => {
