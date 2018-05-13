13dceb712f77e15131e2ff1cf7df951a56510751
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-12 17:00:13 -0700

    ec2

--- server/src/psqlQuery.js
+++ server/src/psqlQuery.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const client = new Pool({ database: 'zaget', host: 'zaget.cvjywnma6qrl.us-west-1.rds.amazonaws.com', user: 'dmytromarchenko1998', password: 'Tiger101998!' });
+const client = new Pool({ database: 'zaget', host: 'arn:aws:rds:us-west-1:350976328477', user: 'dmytromarchenko1998', password: 'Tiger101998!' });
 
 const psqlQuery = (req, res) => {
   const placeId = parseInt(req.params.id, 10);
