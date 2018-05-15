0f3ccc80df96fa1822dcb7977b50ec126b0e374a
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-15 16:51:37 -0700

    weovnerogi

--- client/dist/index.jsx
+++ client/dist/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
 import App from '../src/app.jsx';
+import $ from 'jquery';
 
 export default App;
 
 if (typeof window !== 'undefined') {
-  hydrate(React.createElement(App), document.getElementById('nearby-app'));
+  $.ajax({
+    url: `http://13.57.205.164:3004/api/restaurants/${this.state.id}/nearby`,
+    method: 'GET',
+    success: (data) => {
+      hydrate(React.createElement(App, ), document.getElementById('nearby-app'));  
+    },
+    error: (err) => {
+      console.log('GET Error: ', err);
+    },
+  });
 }
