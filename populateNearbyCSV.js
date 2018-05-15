273710298670cf35fe5d78b0c3e3ebabde69a41b
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-14 19:15:40 -0700

    ec2

--- populateNearbyCSV.js
+++ populateNearbyCSV.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 const { generateNearbyCSV } = require('./populateData.js');
 
-generateNearbyCSV(10000000, fs.createWriteStream('nearby10.csv'), 'utf8', () => {
+generateNearbyCSV(3000000, fs.createWriteStream('nearby3.csv'), 'utf8', () => {
   console.log('done');
 });
--- server/src/psqlQuery.js
+++ server/src/psqlQuery.js
@@ -4,25 +4,21 @@ const client = new Pool({ database: 'zaget', host: 'zaget.cvjywnma6qrl.us-west-1
 
 const psqlQuery = (req, res) => {
   const placeId = parseInt(req.params.id, 10);
-  console.log(placeId);
-  client.query(`select * from businesses where place_id = ${placeId}`, (err, data) => {
-    console.log(data);
-    // console.log([data.rows[0]]);
-  // client.query(`select * from nearby inner join businesses on ${placeId} = businesses.place_id or nearby.nearby1 = businesses.place_id or nearby.nearby2 = businesses.place_id or nearby.nearby3 = businesses.place_id or nearby.nearby4 = businesses.place_id or nearby.nearby5 = businesses.place_id or nearby.nearby6 = businesses.place_id where nearby.place_id = ${placeId}`, (err, data) => {
+  client.query(`select * from nearby inner join businesses on ${placeId} = businesses.place_id or nearby.nearby1 = businesses.place_id or nearby.nearby2 = businesses.place_id or nearby.nearby3 = businesses.place_id or nearby.nearby4 = businesses.place_id or nearby.nearby5 = businesses.place_id or nearby.nearby6 = businesses.place_id where nearby.place_id = ${placeId}`, (err, data) => {
     if (err) {
       res.status(500);
       console.log(err);
     } else {
-      // const nearby = [];
-      // let current;
-      // for (let i = 0; i < 7; i += 1) {
-      //   if (data.rows[i].place_id === placeId) {
-      //     current = data.rows[i];
-      //   } else {
-      //     nearby.push(data.rows[i]);
-      //   }
-      // }
-      // res.send([current, nearby]);
+      const nearby = [];
+      let current;
+      for (let i = 0; i < 7; i += 1) {
+        if (data.rows[i].place_id === placeId) {
+          current = data.rows[i];
+        } else {
+          nearby.push(data.rows[i]);
+        }
+      }
+      res.send([current, nearby]);
       res.send([data.rows[0],[data.rows[0],data.rows[0],data.rows[0],data.rows[0],data.rows[0],data.rows[0]]])
     }
   });
