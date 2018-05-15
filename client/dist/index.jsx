61e4674330343d0cc7383ae65ef8fd368b9fe899
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-15 16:35:58 -0700

    weovnerogi

--- client/dist/index.jsx
+++ client/dist/index.jsx
@@ -9,6 +9,5 @@ if (typeof window !== 'undefined') {
   axios.get(`http://13.57.205.164:3004/api/restaurants/${window.location.href.split('/')[4]}/nearby`)
   .then(data => {
     let info = {currentRestaurant:data[0], nearbyRestaurants:data[1], id:window.location.href.split('/')[4]}
-    hydrate(React.createElement(App, info), document.getElementById('nearby-app'));
   }
 }
