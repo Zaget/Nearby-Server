4012f63c6e3319bf75c6b42ae4d839331d42d584
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-15 16:46:53 -0700

    weovnerogi

--- client/dist/index.jsx
+++ client/dist/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
 import App from '../src/app.jsx';
-import axios from 'axios';
 
 export default App;
 
 if (typeof window !== 'undefined') {
-  
+  hydrate(React.createElement(App), document.getElementById('nearby-app'));
 }
