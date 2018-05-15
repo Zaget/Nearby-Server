5c3579f88a3eddfb10d29b7e07d0663027595a72
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-15 16:45:07 -0700

    weovnerogi

--- client/dist/index.jsx
+++ client/dist/index.jsx
@@ -6,9 +6,5 @@ import axios from 'axios';
 export default App;
 
 if (typeof window !== 'undefined') {
-  axios.get(`http://13.57.205.164:3004/api/restaurants/${window.location.href.split('/')[4]}/nearby`)
-  .then(data => {
-    let info = {currentRestaurant:data[0], nearbyRestaurants:data[1], id:window.location.href.split('/')[4]}
-    hydrate(React.createElement(App, info), document.getElementById('nearby-app'));
-  }
+  
 }
