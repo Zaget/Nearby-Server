b7b93c7270dddb4e9fb979b362652b14b965508d
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-15 17:31:21 -0700

    weovnerogi

--- client/dist/index.jsx
+++ client/dist/index.jsx
@@ -5,6 +5,6 @@ import App from '../src/app.jsx';
 export default App;
 
 if (typeof window !== 'undefined') {
-  const fillerData = {name:'', google_rating: 0, zagat_rating: 0, photos:[105], neighborhood:'', price_level:1, types: ''}
+  const fillerData = {name:'', google_rating: 0, zagat_rating: 0, photos:[], neighborhood:'', price_level:1, types: ''}
   hydrate(React.createElement(App, {currentRestaurant:fillerData, nearbyRestaurants:[fillerData, fillerData, fillerData, fillerData, fillerData, fillerData]}), document.getElementById('nearby-app'));
 }
