f0ede134a3db95964edb1033b89f1a631df98a7f
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-17 13:17:57 -0700

    weofiwnef

--- server/src/psqlQuery.js
+++ server/src/psqlQuery.js
@@ -1,40 +1,10 @@
 const { Pool } = require('pg');
 const redis = require('redis');
+const nr = require('newrelic');
 
 const redisClient = redis.createClient(6379, '13.57.222.179');
 
 const client = new Pool({ database: 'zaget', host: 'zaget.cvjywnma6qrl.us-west-1.rds.amazonaws.com', user: 'dmytromarchenko1998', password: 'Tiger101998!' });
-
-// const getData = (req, res) => {
-//   const placeId = parseInt(req.params.id, 10);
-//   redisClient.get(placeId, (err, reply) => {
-//     data = JSON.parse(reply);
-//     if (data) {
-//       res.send(data);
-//     } else {      
-//       client.query(`select * from nearby inner join businesses on ${placeId} = businesses.place_id or nearby.nearby1 = businesses.place_id or nearby.nearby2 = businesses.place_id or nearby.nearby3 = businesses.place_id or nearby.nearby4 = businesses.place_id or nearby.nearby5 = businesses.place_id or nearby.nearby6 = businesses.place_id where nearby.place_id = ${placeId}`, (err, data) => {
-//         if (err) {
-//           res.status(500);
-//           res.send('not a valid id');
-//           console.log(err);
-//         } else {
-//           const nearby = [];
-//           let current;
-//           for (let i = 0; i < 7; i += 1) {
-//             if (data.rows[i].place_id === placeId) {
-//               current = data.rows[i];
-//             } else {
-//               nearby.push(data.rows[i]);
-//             }
-//           }
-//           const dataStr = JSON.stringify([current, nearby]);
-//           redisClient.set(placeId, dataStr);
-//           res.send([current, nearby]);
-//         }
-//       });
-//     }
-//   });
-// };
 
 const checkRedis = (req, res) => {
   const placeId = parseInt(req.params.id, 10);
@@ -50,8 +20,10 @@ const checkRedis = (req, res) => {
 }
 
 const addToRedis = (id, data) => {
-  console.log('added to redis')
-  redisClient.set(id, data);
+  nr.startSegment('addToRedis', true, () => {
+    console.log('added to redis')
+    redisClient.set(id, data);
+  })
 }
 
 const queryPsql = (id, res) => {
