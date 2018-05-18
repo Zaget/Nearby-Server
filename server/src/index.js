8d51555b0f902e41a7f088f783b6b562bfdbeada
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-18 09:55:47 -0700

    weifubwe

--- server/src/index.js
+++ server/src/index.js
@@ -29,6 +29,10 @@ app.get('/loaderio-84648857ff0fc5a3011e32bc5bd1b98f', (req, res) => {
   res.send('loaderio-84648857ff0fc5a3011e32bc5bd1b98f');
 })
 
+app.get('/loaderio-44e46f07198225a48ab48ed5165ea832', (req, res) => {
+  res.send('loaderio-44e46f07198225a48ab48ed5165ea832');
+})
+
 app.get('/restaurants/:id', (req, res) => {
   const markup = renderToString(React.createElement(App, info));
   res.send(`
