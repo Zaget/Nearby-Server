c0f2855424d287fda12b6c7c5769ce9d64c46e32
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-17 21:54:50 -0700

    wqfiuwenf

--- server/src/psqlQuery.js
+++ server/src/psqlQuery.js
@@ -6,7 +6,7 @@ const psqlQuery = (req, res) => {
   const placeId = parseInt(req.params.id, 10);
   // client.query(`select * from nearby inner join businesses on ${placeId} = businesses.place_id or nearby.nearby1 = businesses.place_id or nearby.nearby2 = businesses.place_id or nearby.nearby3 = businesses.place_id or nearby.nearby4 = businesses.place_id or nearby.nearby5 = businesses.place_id or nearby.nearby6 = businesses.place_id where nearby.place_id = ${placeId}`, (err, data) => {
     client.query(`select * from nearby where place_id = ${placeId}`, (err, data) => {
-      console.log(typeof data);
+      console.log(data.rows);
     })
   //   if (err) {
   //     res.status(500);
