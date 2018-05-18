e29e6bf831324f61a798dc46c933781ccd6a0c62
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-17 22:59:24 -0700

    wefiwebf

--- server/src/psqlQuery.js
+++ server/src/psqlQuery.js
@@ -1,30 +1,43 @@
 const { Pool } = require('pg');
+const redis = require('redis');
 
 const client = new Pool({ database: 'zaget', host: 'zaget.cvjywnma6qrl.us-west-1.rds.amazonaws.com', user: 'dmytromarchenko1998', password: 'Tiger101998!' });
 
+const redisClient = redis.createClient(6379, '13.57.222.179');
+
 const psqlQuery = (req, res) => {
-  const placeId = parseInt(req.params.id, 10);
-  // client.query(`select * from nearby inner join businesses on ${placeId} = businesses.place_id or nearby.nearby1 = businesses.place_id or nearby.nearby2 = businesses.place_id or nearby.nearby3 = businesses.place_id or nearby.nearby4 = businesses.place_id or nearby.nearby5 = businesses.place_id or nearby.nearby6 = businesses.place_id where nearby.place_id = ${placeId}`, (err, data) => {
-  client.query(`select * from nearby where place_id = ${placeId}`, (err, data) => {
-    const ids = Object.values(data.rows[0]);
-    client.query(`select * from businesses where place_id in (${ids[0]}, ${ids[1]}, ${ids[2]}, ${ids[3]}, ${ids[4]}, ${ids[5]}, ${ids[6]})`, (err, data) => {
-      if (err) {
-        res.status(500);
-        res.send('not a valid id');
-        console.log(err);
-      } else {
-        const nearby = [];
-        let current;
-        for (let i = 0; i < 7; i += 1) {
-          if (data.rows[i].place_id === placeId) {
-            current = data.rows[i];
+  redisClient.get(req.params.id, (err, reply) => {
+    if (reply === null) {
+      const placeId = parseInt(req.params.id, 10);
+      client.query(`select * from nearby inner join businesses on ${placeId} = businesses.place_id or nearby.nearby1 = businesses.place_id or nearby.nearby2 = businesses.place_id or nearby.nearby3 = businesses.place_id or nearby.nearby4 = businesses.place_id or nearby.nearby5 = businesses.place_id or nearby.nearby6 = businesses.place_id where nearby.place_id = ${placeId}`, (err, data) => {
+      // client.query(`select * from nearby where place_id = ${placeId}`, (err, data) => {
+      //   const ids = Object.values(data.rows[0]);
+      //   client.query(`select * from businesses where place_id in (${ids[0]}, ${ids[1]}, ${ids[2]}, ${ids[3]}, ${ids[4]}, ${ids[5]}, ${ids[6]})`, (err, data) => {
+          if (err) {
+            res.status(500);
+            res.send('not a valid id');
+            console.log(err);
           } else {
-            nearby.push(data.rows[i]);
+            const nearby = [];
+            let current;
+            for (let i = 0; i < 7; i += 1) {
+              if (data.rows[i].place_id === placeId) {
+                current = data.rows[i];
+              } else {
+                nearby.push(data.rows[i]);
+              }
+            }
+            const idStr = JSON.stringify(placeId);
+            const dataStr = JSON.stringify([current, nearby]);
+            redisClient.set(idStr, dataStr)
+            res.send([current, nearby]);
           }
-        }
-        res.send([current, nearby]);
-      }
-    })
+        // })
+      })
+    } else {
+      const data = JSON.parse(reply);
+      res.send(data);
+    }
   })
 };
 
