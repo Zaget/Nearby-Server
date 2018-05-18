4b17fec53f33607c5885ea93c4ee2420cf2efd3e
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-17 21:49:09 -0700

    wqfiuwenf

--- server/src/psqlQuery.js
+++ server/src/psqlQuery.js
@@ -8,23 +8,23 @@ const psqlQuery = (req, res) => {
     client.query(`select * from nearby where place_id = ${placeId}`, (err, data) => {
       console.log(data);
     })
-    if (err) {
-      res.status(500);
-      res.send('not a valid id');
-      console.log(err);
-    } else {
-      const nearby = [];
-      let current;
-      for (let i = 0; i < 7; i += 1) {
-        if (data.rows[i].place_id === placeId) {
-          current = data.rows[i];
-        } else {
-          nearby.push(data.rows[i]);
-        }
-      }
-      res.send([current, nearby]);
-    }
-  });
+  //   if (err) {
+  //     res.status(500);
+  //     res.send('not a valid id');
+  //     console.log(err);
+  //   } else {
+  //     const nearby = [];
+  //     let current;
+  //     for (let i = 0; i < 7; i += 1) {
+  //       if (data.rows[i].place_id === placeId) {
+  //         current = data.rows[i];
+  //       } else {
+  //         nearby.push(data.rows[i]);
+  //       }
+  //     }
+  //     res.send([current, nearby]);
+  //   }
+  // });
 };
 
 module.exports = psqlQuery;
