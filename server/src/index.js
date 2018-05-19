93f55f68659f579dd4494e816de73674362e0adc
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-18 17:44:33 -0700

    wqfiuqwbef

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,7 @@ const query = require('./psqlQuery.js');
 const { renderToString } = require('react-dom/server');
 const App = require('../../client/dist/bundle.js').default;
 const React = require('react');
+const html = require('./template.js')
 
 const app = express();
 
@@ -55,23 +56,7 @@ app.get('/loaderio-09e9734e71fc5f4cd23b0acdd6142942', (req, res) => {
 
 app.get('/restaurants/:id', (req, res) => {
   const markup = renderToString(React.createElement(App, info));
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Apateez Recommendations</title>
-        <link rel="icon" href="https://s3-us-west-1.amazonaws.com/apateezassets/apateez-logo-small-red.jpeg" type="image/x-icon">
-        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-        <link rel="stylesheet" href="styles.css">
-      </head>
-      <body>
-        <div id="nearby-app">${markup}</div>
-        <script src="bundle.js"></script>
-      </body>
-    </html>
-  `);
+  res.send(html(markup));
 });
 
 module.exports = app;
